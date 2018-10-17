b09d8f8e10b01c27f9edeacba8d45a573e27fb93
damunza <danidamunza@gmail.com>
2018-10-17 11:45:22 +0300

    made the view function for the homepage

--- app/views.py
+++ app/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import *
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+def home(request):
+    '''
+    returns the homepage of the application
+    '''
+    return render(request, 'index.html')
