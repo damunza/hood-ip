df337c560678af4bb3b8659bae1101180c261403
damunza <danidamunza@gmail.com>
2018-10-17 15:11:42 +0300

    made a model for businesses

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Business(models.Model):
+    business_name= models.CharField(max_length=50)
+    email = models.EmailField()
+    owner = models.ForeignKey(User,on_delete=models.CASCADE)
+    neighbourhood = models.ForeignKey('app.Hood', on_delete=models.CASCADE, related_name='jirani')
