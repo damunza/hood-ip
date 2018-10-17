9c5b742efdc5b95a854db4f9fa20fa5088b62729
damunza <danidamunza@gmail.com>
2018-10-17 15:29:59 +0300

    made the hood model

--- app/models.py
+++ app/models.py
@@ -8,3 +8,24 @@ class Business(models.Model):
     email = models.EmailField()
     owner = models.ForeignKey(User,on_delete=models.CASCADE)
     neighbourhood = models.ForeignKey('app.Hood', on_delete=models.CASCADE, related_name='jirani')
+
+    def __str__(self):
+        return self.business_name
+
+class Hood(models.Model):
+    location_choices = (
+        (1, 'Nairobi'),
+        (2, 'Kajiado'),
+        (3, 'Mombasa'),
+        (4, 'Kilifi'),
+        (5, 'kiambu'),
+    )
+    name= models.CharField(max_length=50)
+    location = models.CharField(max_length=50, choices=location_choices)
+    occupants = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+
+
