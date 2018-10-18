3e76af97eba0c4d58420b464bdfc30030013758b
damunza <danidamunza@gmail.com>
2018-10-18 12:07:48 +0300

    added a view function for the new profile

--- app/views.py
+++ app/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
 from django.contrib.auth.decorators import login_required
+from .forms import *
 
 # Create your views here.
-
+@login_required(login_url='/accounts/login/')
 def home(request):
     '''
     returns the homepage of the application
@@ -17,5 +18,26 @@ def profile(request,name):
 
     return render(request, 'profile.html', {'content': human})
 
+@login_required(login_url='/accounts/login/')
+def new_resident(request, operation, town):
+    current_user = request.user
+    hood = get_object_or_404(Hood, name=town)
+    if request.method == 'POST':
+        form = NewResidentForm(request.POST, request.FILES)
+        if form.is_valid():
+            resident = form.save(commit=False)
+            resident.name = current_user
+            resident.home = hood
+            resident.save()
+        return redirect('home')
+    else:
+        form = NewResidentForm()
 
+    if operation == 'join':
+        hood.occupants +=1
+        hood.save()
+
+        return redirect('home')
+
+    return render(request, 'new_being.html', {'form': form})
 
